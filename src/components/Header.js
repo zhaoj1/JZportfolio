4114d046fb725b34ff7490048d1f963b2f29de12
github username <justinzhao265@gmail.com>
2020-10-20 13:03:51 -0400

    scroll

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <div className="header">
+      Justin Zhao ABOUT PROJECTS CONTACT
+    </div>
+  );
+}
+
+export default Header;

894fd014af9c5ec6c1d376ce591121c29fb76886
github username <justinzhao265@gmail.com>
2020-10-20 14:26:21 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ import '../App.css';
 function Header() {
   return (
     <div className="header">
-      Justin Zhao ABOUT PROJECTS CONTACT
+      <span className='name'>Justin Zhao</span> 
+      <div className='links'>
+        <span className='link'>ABOUT</span>
+        <span className='link'>PROJECTS</span>
+        <span className='link'>CONTACT</span>
+      </div>
     </div>
   );
 }

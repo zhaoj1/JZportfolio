1471f37dc27bf3c06a9a4248b6476e87949482ab
github username <justinzhao265@gmail.com>
2020-10-23 16:55:35 -0400

    full page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
+import ReactFullpage from '@fullpage/react-fullpage';
 
-function App() {
-  return (
+export default class App extends React.Component{
+
+  return (){
     <div className="App">
       <Header />
+      <ReactFullpage
+        licenseKey = {'YOUR_KEY_HERE'}
+        scrollingSpeed = {1000}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <p>Section 1 (welcome to fullpage.js)</p>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Click me to move down
+                </button>
+              </div>
+              <div className="section">
+                <p>Section 2</p>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </div>
-  );
+  };   
 }
-
-export default App;

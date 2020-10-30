ff95b9979d09978cdadc9d2124dfc44b002ab4c7
github username <justinzhao265@gmail.com>
2020-10-30 17:44:25 -0400

    changed to different fullpage lib

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 import React from 'react';
 import './App.css';
 import Contact from './components/Contact.js';
-import ReactFullpage from '@fullpage/react-fullpage';
+import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage'
 
 export default class App extends React.Component{
 
   render(){
     return (
-      <div className="App">
-        <ReactFullpage
-          scrollingSpeed = {1000}
-          fixedElements =  '.header'
-          anchors={['','about','projects', 'contact']}
-          render={({ state, fullpageApi }) => {
-            return (
-              <ReactFullpage.Wrapper>
-                <div className="header">
-                  <span 
-                    className='name'
-                    onClick={() => fullpageApi.moveTo('')}
-                  >Justin Zhao</span> 
-                  <div className='links'>
-                    <span 
-                      className='link'
-                      onClick={() => fullpageApi.moveTo('about')}
-                    >ABOUT</span>
-                    <span 
-                      className='link'
-                      onClick={() => fullpageApi.moveTo('projects')}
-                    >PROJECTS</span>
-                    <span 
-                      className='link'
-                      onClick={() => fullpageApi.moveTo('contact')}
-                    >CONTACT</span>
-                  </div>
-                </div>
-                <div className="section">
-                  <p>HOME</p>
-                </div>
-                <div className="section">
-                  <p>ABOUT</p>
-                </div>
-                <div className="section">
-                  <p>PROJECTS</p>
-                </div>
-                <div className="section">
-                  <Contact />
-                </div>
-              </ReactFullpage.Wrapper>
-            );
-          }}
-        />
+      <div className="App">   
+        <Fullpage>
+          <div className="header">
+            <span 
+              className='name'
+            >Justin Zhao</span> 
+            <div className='links'>
+              <span 
+                className='link'
+              >ABOUT</span>
+              <span 
+                className='link'
+              >PROJECTS</span>
+              <span 
+                className='link'
+              >CONTACT</span>
+            </div>
+          </div>
+          <FullPageSections >
+            <FullpageSection style={{'display':'flex'}}>
+              <p style={{'margin':'auto'}}>HOME</p>
+            </FullpageSection>
+            <FullpageSection style={{'display':'flex'}}>
+              <p style={{'margin':'auto'}}>ABOUT</p>
+            </FullpageSection>
+            <FullpageSection style={{'display':'flex'}}>
+              <p style={{'margin':'auto'}}>PROJECTS</p>
+            </FullpageSection>
+            <FullpageSection style={{'display':'flex'}}>
+              <Contact />
+            </FullpageSection>
+          </FullPageSections>
+        </Fullpage>
       </div>
     );   
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,30 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import '../App.css';
 import github from '../assets/GitHub-Mark-120px-plus.png'
 import linkedin from '../assets/LI-In-Bug.png'
 
 function Contact() {
 
-  // function copyToClipboard(){
-  //   textAreaRef.current.select()
-  //   document.execCommand('copy')
-  // }
-
   return (
     <div className="contact">
-      <p>If you would like to reach out to me, please shoot me an email at 
-        <a 
-          id='email' 
-          href={`mailto:justinzhao265@gmail.com`} 
-          value='justinzhao265@gmail.com'
-        > justinzhao265@gmail.com</a>
-        {/* <button onClick={copyToClipboard}>copy</button> */}
-         and I will get back to you ASAP!
-      </p>
-      <p>Otherwise, let's connect!</p>
-      <div className='contact-links-container'>
-        <a href='https://www.linkedin.com/in/justin-zh/' target="_blank" className='contact-links' rel="noopener noreferrer" >
-          <img src={linkedin} height='80' alt='LinkedIn' ></img>  
-        </a>
-        <a href='https://github.com/zhaoj1' target="_blank" className='contact-links' rel="noopener noreferrer" >
-          <img src={github} height='80' alt='GitHub' ></img>  
-        </a>
+      <div className='contact-content'>
+        <p>If you would like to reach out to me, please shoot me an email at 
+          <a 
+            id='email' 
+            href={`mailto:justinzhao265@gmail.com`} 
+            value='justinzhao265@gmail.com'
+          > justinzhao265@gmail.com</a>
+          and I will get back to you ASAP!
+        </p>
+        <p>Otherwise, let's connect!</p>
+        <div className='contact-links-container'>
+          <a href='https://www.linkedin.com/in/justin-zh/' target="_blank" className='contact-links' rel="noopener noreferrer" >
+            <img src={linkedin} height='80' alt='LinkedIn' ></img>  
+          </a>
+          <a href='https://github.com/zhaoj1' target="_blank" className='contact-links' rel="noopener noreferrer" >
+            <img src={github} height='80' alt='GitHub' ></img>  
+          </a>
+        </div>
       </div>
     </div>
   );

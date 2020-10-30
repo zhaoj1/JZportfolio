0cb53f035529669f98194a8f0c7e9cc4847f56c2
github username <justinzhao265@gmail.com>
2020-10-30 17:05:04 -0400

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,35 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import '../App.css';
 import github from '../assets/GitHub-Mark-120px-plus.png'
 import linkedin from '../assets/LI-In-Bug.png'
 
 function Contact() {
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  // function copyToClipboard(){
+  //   textAreaRef.current.select()
+  //   document.execCommand('copy')
+  // }
 
   return (
     <div className="contact">
-      <p>If you would like to reach out to me, please shoot me an email at <a href={`mailto:justinzhao265@gmail.com`} > justinzhao265@gmail.com</a> and I will get back to you ASAP!</p>
+      <p>If you would like to reach out to me, please shoot me an email at 
+        <a 
+          id='email' 
+          href={`mailto:justinzhao265@gmail.com`} 
+          value='justinzhao265@gmail.com'
+        > justinzhao265@gmail.com</a>
+        {/* <button onClick={copyToClipboard}>copy</button> */}
+         and I will get back to you ASAP!
+      </p>
       <p>Otherwise, let's connect!</p>
-      <a href='https://www.linkedin.com/in/justin-zh/' target="_blank">
-        <img src={linkedin} height='50' ></img>  
-      </a>
-      <a href='https://github.com/zhaoj1' target="_blank">
-        <img src={github} height='50' ></img>  
-      </a>
+      <div className='contact-links-container'>
+        <a href='https://www.linkedin.com/in/justin-zh/' target="_blank" className='contact-links' rel="noopener noreferrer" >
+          <img src={linkedin} height='80' alt='LinkedIn' ></img>  
+        </a>
+        <a href='https://github.com/zhaoj1' target="_blank" className='contact-links' rel="noopener noreferrer" >
+          <img src={github} height='80' alt='GitHub' ></img>  
+        </a>
+      </div>
     </div>
   );
 }

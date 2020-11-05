49bb7c7b8f7b152eaa18d42a3a88699662c09dd3
github username <justinzhao265@gmail.com>
2020-11-05 18:23:37 -0500

    tech

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import '../App.css';
 import Tech from './Tech.js';
+
 import react from '../assets/react.png'
 import rails from '../assets/rails.png'
 import photoshop from '../assets/Adobe_Photoshop_CC_icon.svg'
 import django from '../assets/django.svg'
+import git from '../assets/Git_icon.svg'
+import npm from '../assets/Npm-logo.svg'
 import javascript from '../assets/javascript.png'
 import python from '../assets/python.png'
 import html from '../assets/HTML5_logo_and_wordmark.svg'
 import css from '../assets/CSS3_logo_and_wordmark.svg'
+import mysql from '../assets/mysql.png'
+import postgresql from '../assets/postgresql.svg'
 
 const technologies = [
   {name: 'React.js', img: react},
   {name: 'Ruby on Rails', img: rails},
   {name: 'Photoshop', img: photoshop},
   {name: 'Django', img: django},
+  {name: 'Git', img: git},
+  {name: 'NPM', img: npm},
   {name: 'Javascript', img: javascript},
   {name: 'Python', img: python},
   {name: 'HTML5', img: html},
   {name: 'CSS', img: css},
+  {name: 'MySQL', img: mysql},
+  {name: 'PostgreSQL', img: postgresql},
 ]
 
 function About() {
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -5,7 +5,7 @@ function Tech(props) {
 
   return (
     <div className="tech">
-      <img className='tech-img' src={props.img} alt='react' height={100} />
+      <img className='tech-img' src={props.img} alt={props.name} height={100} width={100} />
       <h3 className='tech-name' >{props.name}</h3>
     </div>
   );
